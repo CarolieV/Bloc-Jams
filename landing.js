dcb47032762682d278f415b061c96fc8571913dc
CarolieV <carolie.ventola98@gmail.com>
2017-04-24 15:32:00 -0600

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,5 +1,8 @@
 <script>
-               var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {            
+  var animatePoints = function() {
 
                  var points = document.getElementsByClassName('point');
 
@@ -17,7 +20,18 @@
 
              };
  window.onload = function() {
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
       window.addEventListener('scroll', function(event) {
          console.log(event);
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+
      });
          </script>
