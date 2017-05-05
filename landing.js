9f018854fa6b5fea6c493af9adc1ee66e625bcab
CarolieV <carolie.ventola98@gmail.com>
2017-05-05 08:22:40 -0600

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,33 +1,30 @@
 <script>
- var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {            
-  var animatePoints = function() {
+ var animatePoints = function() {
+  
+  var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+        
+   $.each($('.point'), revealPoint);
 
-                 var points = document.getElementsByClassName('point');
-
-                 function revealPoint(point); {
-                     point.style.opacity = 1;
-                     point.style.transform = "scaleX(1) translateY(0)";
-                     point.style.msTransform = "scaleX(1) translateY(0)";
-                     point.style.WebkitTransform = "scaleX(1) translateY(0)";
-                 }
-   forEach (points,revealPoint)             
 
              };
- window.onload = function() {
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+ $(window).load(function() {
+     if ($(window).height() > 950) {
+         animatePoints();
      }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-      window.addEventListener('scroll', function(event) {
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+  
+     $(window).scroll(function(event) {
          console.log(event);
          console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+       if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
-
      });
+    });
          </script>
